a44aeb77251a2c154e45aa19d76ff4ceb25b52eb
Ned Batchelder <ned@nedbatchelder.com>
2019-12-01 10:36:58 -0500

    Day 1, part 2

--- day01.py
+++ day01.py
@@ -14,10 +14,37 @@ def fuel_for_module(mass):
 def test_fuel_for_module(mass, fuel):
     assert fuel_for_module(mass) == fuel
 
+def fuel_for_modules(modules):
+    return sum(map(fuel_for_module, modules))
+
 if __name__ == "__main__":
     with open("day01_input.txt") as f:
         modules = list(map(int, f))
 
-    all_fuel = sum(map(fuel_for_module, modules))
+    all_fuel = fuel_for_modules(modules)
 
     print(f"Part 1: the sum of the fuel requirements is {all_fuel}")
+
+
+def fuel_for_module_and_fuel(mass):
+    total = 0
+    fuel = fuel_for_module(mass)
+    while fuel > 0:
+        total += fuel
+        fuel = fuel_for_module(fuel)
+    return total
+
+@pytest.mark.parametrize("mass, fuel", [
+    (14, 2),
+    (1969, 966),
+    (100756, 50346),
+])
+def test_fuel_for_modules_and_fuel(mass, fuel):
+    assert fuel_for_module_and_fuel(mass) == fuel
+
+def fuel_for_modules_and_fuel(modules):
+    return sum(map(fuel_for_module_and_fuel, modules))
+
+if __name__ == "__main__":
+    total_fuel = fuel_for_modules_and_fuel(modules)
+    print(f"Part 2: the sum of the fuel requirements including the fuel is {total_fuel}")
