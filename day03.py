218635115c84985b39c4ab3c34f4cfdc1c084581
Ned Batchelder <ned@nedbatchelder.com>
2019-12-03 07:47:19 -0500

    Day 3, part 2

--- day03.py
+++ day03.py
@@ -52,3 +52,39 @@ def part1():
 
 if __name__ == "__main__":
     part1()
+
+# Part 2
+
+def points_on_wire_with_steps(wire_spec):
+    """Yields (x, y, steps)."""
+    x, y, steps = 0, 0, 0
+    for (dx, dy), distance in parse_wire(wire_spec):
+        for _ in range(distance):
+            x += dx; y += dy; steps += 1
+            yield x, y, steps
+
+def wire_log(spec):
+    """Return a dict mapping points to number of steps."""
+    return {(x, y): steps for x, y, steps in points_on_wire_with_steps(spec)}
+
+def fewest_steps_to_intersection(spec1, spec2):
+    log1 = wire_log(spec1)
+    log2 = wire_log(spec2)
+    intersections = set(log1) & set(log2)
+    steps = min(log1[pt] + log2[pt] for pt in intersections)
+    return steps
+
+@pytest.mark.parametrize("spec1, spec2, steps", [
+    ("R8,U5,L5,D3", "U7,R6,D4,L4", 30),
+    ("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 610),
+    ("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 410),
+])
+def test_fewest_steps_to_intersection(spec1, spec2, steps):
+    assert fewest_steps_to_intersection(spec1, spec2) == steps
+
+def part2():
+    steps = fewest_steps_to_intersection(*specs_from_file("day03_input.txt"))
+    print(f"Part 2: fewest steps to intersection is {steps}")
+
+if __name__ == "__main__":
+    part2()
