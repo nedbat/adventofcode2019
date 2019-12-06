1e66d22810dcbe425ddb9d5f4566625627340b3a
Ned Batchelder <ned@nedbatchelder.com>
2019-12-06 06:25:58 -0500

    Day 6, part 2

--- day06.py
+++ day06.py
@@ -43,3 +43,36 @@ def part1():
 
 if __name__ == "__main__":
     part1()
+
+TEST2 = TEST + """\
+K)YOU
+I)SAN
+""".splitlines()
+
+def path_to_node(orbits, node):
+    around = orbits.get(node)
+    if around is not None:
+        getting_here = path_to_node(orbits, around)
+    else:
+        getting_here = set()
+    return getting_here | {node}
+
+def transfers(orbits, n1, n2):
+    p1 = path_to_node(orbits, n1)
+    p2 = path_to_node(orbits, n2)
+    return len(p1) + len(p2) - 2 * len(p1 & p2) - 2
+
+def tranfers_me_to_santa(lines):
+    orbits = build_orbits(lines)
+    return transfers(orbits, "YOU", "SAN")
+
+def test_transfers():
+    assert tranfers_me_to_santa(TEST2) == 4
+
+def part2():
+    with open("day06_input.txt") as f:
+        transfers = tranfers_me_to_santa(f)
+    print(f"Part 2: orbital transfers required is {transfers}")
+
+if __name__ == "__main__":
+    part2()
