d62ad7eae33a52a11c65728e3559a83115565941
Ned Batchelder <ned@nedbatchelder.com>
2019-12-21 17:51:31 -0500

    Day 21, part 2

--- day21.py
+++ day21.py
@@ -3,8 +3,9 @@
 from intcode import IntCode, program_from_file
 
 class AsciiComputer:
-    def __init__(self, program, input):
+    def __init__(self, program, label, input):
         self.cpu = IntCode(program, self.input_fn, self.output_fn)
+        self.label = label
         self.input = []
         self.store_input(input)
     
@@ -20,17 +21,18 @@ class AsciiComputer:
         if 0 <= val < 128:
             print(chr(val), end='', flush=True)
         else:
-            print(f"Numeric output: {val}")
+            print(f"{self.label}: hull damage is: {val}")
 
     def run(self):
+        print(f"--- {self.label} ---------")
         self.cpu.run()
 
-def this_computer(input):
-    return AsciiComputer(program_from_file("day21_input.txt"), input)
+def this_computer(label, input):
+    return AsciiComputer(program_from_file("day21_input.txt"), label, input)
 
 if __name__ == "__main__":
     # Jump if ((not A) and D), or (D and not C)
-    this_computer("""\
+    this_computer("Part 1", """\
 NOT C J
 AND D J
 NOT A T
@@ -38,3 +40,25 @@ AND D T
 OR T J
 WALK
 """).run()
+
+    seen_patterns = [
+        "##=##.=..#=###=##",
+        "=###=.#.=..#=.##=",
+        "##=##.=.#.=#..=##",
+        #   ABCDEFGHI
+    ]
+    this_computer("Part 2", 
+# Jump if (D) and (E or H) and not (A and B and C and D)
+"""\
+OR H J
+OR E J
+AND D J
+OR A T
+AND B T
+AND C T
+AND D T
+NOT T T
+AND T J
+RUN
+"""
+).run()
