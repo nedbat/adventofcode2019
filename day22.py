7d83e061dbb9ba4f78d6b5f7eac4de771b8f7f7c
Ned Batchelder <ned@nedbatchelder.com>
2019-12-22 11:13:43 -0500

    Day 22, tweak part 1 to be usable for part 2

--- day22.py
+++ day22.py
@@ -9,7 +9,7 @@ import pytest
 
 def make_program(shuffle_process):
     code = []
-    code.append("def doit(deck):")
+    code.append("def doit(*args):")
     for line in shuffle_process.splitlines():
         line = line.strip()
         if not line:
@@ -17,15 +17,15 @@ def make_program(shuffle_process):
         op, amt = re.search(r"^(.*[a-z]) ?(-?\d*)$", line).groups()
         op = op.replace(" ", "_")
         amt = int(amt or "0")
-        code.append(f"  deck = {op}({amt}, deck)")
-    code.append("  return deck")
+        code.append(f"  args = {op}({amt}, *args)")
+    code.append("  return args")
     return "\n".join(code) + "\n"
 
 def deal_into_new_stack(_, deck):
-    return deck[::-1]
+    return (deck[::-1],)
 
 def cut(amt, deck):
-    return deck[amt:] + deck[:amt]
+    return ((deck[amt:] + deck[:amt]),)
 
 def deal_with_increment(amt, deck):
     new_deck = [None] * len(deck)
@@ -34,9 +34,9 @@ def deal_with_increment(amt, deck):
         new_deck[new_i] = card
         new_i += amt
         new_i %= len(deck)
-    return new_deck
+    return (new_deck,)
 
-def run_process(process, deck):
+def run_process(process, *args):
     prog = make_program(process)
     globs = {
         'deal_into_new_stack': deal_into_new_stack,
@@ -44,10 +44,10 @@ def run_process(process, deck):
         'deal_with_increment': deal_with_increment,
     }
     exec(prog, globs, globs)
-    deck = globs['doit'](deck)
-    return deck
+    args = globs['doit'](*args)
+    return args
 
-@pytest.mark.parametrize("process, result", [
+TEST_DATA = [
     ("deal into new stack\n", [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]),
     ("cut 3\n", [3, 4, 5, 6, 7, 8, 9, 0, 1, 2]),
     ("cut -4\n", [6, 7, 8, 9, 0, 1, 2, 3, 4, 5]),
@@ -87,15 +87,20 @@ def run_process(process, deck):
         """,
         [9, 2, 5, 8, 1, 4, 7, 0, 3, 6],
     ),
-])
+]
+
+@pytest.mark.parametrize("process, result", TEST_DATA)
 def test_run_process(process, result):
-    assert run_process(process, list(range(10))) == result
+    assert run_process(process, list(range(10))) == (result,)
 
 if __name__ == "__main__":
     deck = list(range(10007))
     with open("day22_input.txt") as f:
         process = f.read()
-    deck = run_process(process, deck)
+    (deck,) = run_process(process, deck)
     i2019 = deck.index(2019)
     print(f"Part 1: 2019 is at {i2019}")
+
+# Part 2: closed-form tracking of where a card ends up.
+
 
